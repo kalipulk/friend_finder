28b80895169e4333cf4ca67c31d55a52d65ed42a
Kali Pulkkinen <kapulkkinen@gmail.com>
2020-01-29 17:24:09 -0500

    adding potential friends data

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friends = [
+    {
+        name: "Richard Hendricks",
+        image: "../views/images/Richard_Hendricks.png",
+        score: [
+            3,
+            4,
+            1,
+            2,
+            2,
+            5,
+            1,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Jared Dunn",
+        image: "../views/images/Jared_Dunn.png",
+        score: [
+            1,
+            5,
+            4,
+            3,
+            3,
+            1,
+            5,
+            4,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Dinesh",
+        image: "../views/images/Dinesh.png",
+        score: [
+            5,
+            5,
+            5,
+            1,
+            2,
+            4,
+            3,
+            1,
+            5,
+            2
+        ]
+    },
+    {
+        name: "Gilfoyle",
+        image: "../views/images/Gilfoyle.png",
+        score: [
+            1,
+            1,
+            2,
+            4,
+            4,
+            5,
+            5,
+            4,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Jian Yang",
+        image: "../views/images/Jian_Yang.png",
+        score: [
+            4,
+            5,
+            1,
+            1,
+            3,
+            4,
+            3,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Gavin Belson",
+        image: "../views/images/Gavin_Belson.png",
+        score: [
+            2,
+            2,
+            4,
+            3,
+            1,
+            5,
+            5,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Erlich Bachman",
+        image: "../views/images/Erlich_Bachman.png",
+        score: [
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Monica",
+        image: "../views/images/Monica.png",
+        score: [
+            3,
+            4,
+            3,
+            4,
+            3,
+            4,
+            4,
+            3,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Laurie Bream",
+        image: "../views/images/Laurie_Bream.png",
+        score: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Nelson Bighetti",
+        image: "../views/images/Big_Head.png",
+        score: [
+            3,
+            4,
+            1,
+            2,
+            2,
+            5,
+            1,
+            3,
+            4,
+            5
+        ]
+    }
+];
+
+module.exports = friends;
---
+++ friend_server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/app/public"));
+
+require('/app/routing/htmlRoutes')(app);
+require('/app/routing/apiRoutes')(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
