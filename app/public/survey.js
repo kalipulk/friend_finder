6078ed616221edf26e409f15ef016cb535db8a3b
Kali Pulkkinen <kapulkkinen@gmail.com>
2020-02-02 16:10:15 -0500

    adding matching friend

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,31 +1,36 @@
-$(document).ready(function() {
- 
-$("#submit-button").on("click", function(event){
-    console.log("you got clicked");
-    var newFriend = {
-        name: $("#name-input").val(),
-        image: $("#image-input").val(),
-        score: [ 
-            $("#q1").val(),
-            $("#q2").val(),
-            $("#q3").val(),
-            $("#q4").val(),
-            $("#q5").val(),
-            $("#q6").val(),
-            $("#q7").val(),
-            $("#q8").val(),
-            $("#q9").val(),
-            $("#q10").val()
-        ]
-    }
-    console.log(newFriend);
-    $.ajax({
-        url:"/api/friends",
-        type: "post",
-        data: newFriend
-    }).then(function(){
-        
+$(document).ready(function () {
+
+    let matchContainer = $(".best-match");
+    let matchName = $(".best-match-name");
+    let matchImage = $(".best-match-img");
+    $("#submit-button").on("click", function (event) {
+        event.preventDefault();
+        console.log("you got clicked");
+        var newFriend = {
+            name: $("#name-input").val(),
+            image: $("#image-input").val(),
+            score: [
+                $("#q1").val(),
+                $("#q2").val(),
+                $("#q3").val(),
+                $("#q4").val(),
+                $("#q5").val(),
+                $("#q6").val(),
+                $("#q7").val(),
+                $("#q8").val(),
+                $("#q9").val(),
+                $("#q10").val()
+            ]
+        }
+        console.log(newFriend);
+        $.ajax({
+            url: "/api/friends",
+            type: "post",
+            data: newFriend
+        }).then(function (data) {
+            matchImage.attr("src", data.image);     
+            matchName.html(data.name);                   
+        })
     })
-})
 
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,34 @@
+var friends = require("../data/friends")
+
 module.exports= function(app) {
-    var path = require("path");
-    var friends = require("../data/friends")
     app.get("/api/friends", function(req, res){
         res.json(friends);
     })
     app.post("/api/friends", function(req, res){
-        // res.json(friends);
-        friends.push(req.body);
+       var totalDiff; 
+       var bestMatch = {
+           name: "",
+           image: "",
+           score: Infinity
+       }
+       var userData = req.body;
+       var userScores = userData.score;
+       for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            totalDiff = 0;
+            for (var j = 0; j < currentFriend.score.length; j++) {
+                var currentFriendScore = currentFriend.score[j];
+                var currentUserScore = userScores[j];
+                totalDiff += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore) )
+            } 
+            console.log(currentUserScore, currentFriendScore, currentFriend.name);
+            if (totalDiff <= bestMatch.score) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.image = currentFriend.image;
+                bestMatch.score = currentFriend.score;
+            } 
+       }
+       friends.push(userData);
+       res.json(bestMatch);
     })
 }
